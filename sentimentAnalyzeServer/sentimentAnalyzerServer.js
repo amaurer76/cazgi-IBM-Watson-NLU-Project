f895c0d2e7d14bdcc81cfbfcfab0877c2f922aac
Andrei Maurer <andreimaurer1@gmail.com>
2021-05-02 02:15:18 +0000

    step 4

--- sentimentAnalyzeServer/sentimentAnalyzerServer.js
+++ sentimentAnalyzeServer/sentimentAnalyzerServer.js
@@ -3,6 +3,17 @@ const app = new express();
 const dotenv = require('dotenv');
 dotenv.config();
 
+const analyzeParams = {
+  'url': 'www.ibm.com',
+  'features': {
+    'entities': {
+      'emotion': true,
+      'sentiment': true,
+      'limit': 2,
+    }
+}
+};
+
 function getNLUInstance() {
     let api_key = process.env.API_KEY;
     let api_url = process.env.API_URL;
@@ -12,13 +23,13 @@ const { IamAuthenticator } = require('ibm-watson/auth');
 const naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
   version: '2020-08-01',
   authenticator: new IamAuthenticator({
-    apikey: '{apikey}',
+    apikey: api_key,
   }),
-  serviceUrl: '{url}',
+  serviceUrl: api_url,
 });
 return naturalLanguageUnderstanding;
 }
-app.use(express.static('client'))
+app.use(express.static('client'));
 
 const cors_app = require('cors');
 app.use(cors_app());
@@ -29,7 +40,7 @@ app.get("/",(req,res)=>{
 
 app.get("/url/emotion", (req,res) => {
 
-    return res.send({"happy":"90","sad":"10"});
+    return getNLUInstance().analyze(analyzeParams, req.query.url);
 });
 
 app.get("/url/sentiment", (req,res) => {
@@ -37,11 +48,28 @@ app.get("/url/sentiment", (req,res) => {
 });
 
 app.get("/text/emotion", (req,res) => {
-    return res.send({"happy":"10","sad":"90"});
+console.log(req);
+getNLUInstance().analyze(analyzeParams, req.query.text)
+  .then(analysisResults => {
+    console.log(JSON.stringify(analysisResults, null, 2));
+        res.end(JSON.stringify(analysisResults, null, 2));
+        return "hey";
+  })
+  .catch(err => {
+    console.log('error:', err);
+  });
+    return ;
 });
 
 app.get("/text/sentiment", (req,res) => {
-    return res.send("text sentiment for "+req.query.text);
+getNLUInstance().analyze(analyzeParams, req.query.text)
+  .then(analysisResults => {
+    console.log(JSON.stringify(analysisResults, null, 2));
+    return analysisResults;
+  })
+  .catch(err => {
+    console.log('error:', err);
+  });
 });
 
 let server = app.listen(8080, () => {
